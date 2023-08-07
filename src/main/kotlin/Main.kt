b76e59df8590d458a2d297531f87b3230e51de9e
Yuriy Chernov <Chernov.y.12@gmail.com>
2023-08-08 00:26:43 +0300

    Second Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,38 @@
+const val TYPE_MASTERCARD = "MasterCard"
+const val TYPE_MAESTRO = "Maestro"
+const val TYPE_VISA = "Visa"
+const val TYPE_MIR = "Mir"
+const val TYPE_VK_PAY = "VK Pay"
+const val ERROR_WRONG_TYPE = -1.0
+const val ERROR_LIMIT = -2.0
+
+
 fun main() {
-    var money = 80000.00
-    val typeOfCard = "VK Pay"
-    val transactionMaestroMasterCardMin = 300
-    val transactionMaestroMasterCardMax = 75000
-    val perCommissionVisaMir = 0.0075
-    val minCommissionVisaMir = 35
+    println(commission(typeCard = TYPE_VISA, 100_000, 0))
+}
 
-    val commissionMaestroMasterCard = (money * 0.006) + 20
-    val resultMaestroMasterCard = money + commissionMaestroMasterCard
-    val maestroMasterCard = if (money >= transactionMaestroMasterCardMin && money <= transactionMaestroMasterCardMax) {
-        money
-    } else resultMaestroMasterCard
+fun commission(typeCard: String, money: Int, previous: Int): Any {
+    return when (typeCard) {
+        TYPE_MASTERCARD -> if (money > 150_000 || money + previous > 600_000) {
+            ERROR_LIMIT
+        } else if (money in 300..75_000) 0.00 else (money * 0.006) + 20
 
-    val commissionVisaMir = money * perCommissionVisaMir
-    val resultVisaMir = money + commissionVisaMir
-    val resultVisaMir2 = money + minCommissionVisaMir
-    val visaMir = if (commissionVisaMir < minCommissionVisaMir) {
-        resultVisaMir2
-    } else resultVisaMir
+        TYPE_MAESTRO -> if (money > 150_000 || money + previous > 600_000) {
+            ERROR_LIMIT
+        } else if (money in 300..75_000) 0.00 else (money * 0.006) + 20
 
-    val result = when (typeOfCard) {
-        "Maestro", "MasterCard" -> maestroMasterCard
-        "Visa", "Mir" -> visaMir
-        "VK Pay" -> money
-        else -> "Карта не найдена"
+        TYPE_VISA -> if (money > 150_000 || money + previous > 600_000) {
+            ERROR_LIMIT
+        } else if ((money * 0.075) < 35) (money + 35) else money + (money * 0.075)
+
+        TYPE_MIR -> if (money > 150_000 || money + previous > 600_000) {
+            ERROR_LIMIT
+        } else if ((money * 0.075) < 35) (money + 35) else money + (money * 0.075)
+
+        TYPE_VK_PAY -> if (money > 15_000 || money + previous > 40_000) {
+            ERROR_LIMIT
+        } else money
+
+        else -> ERROR_WRONG_TYPE
     }
-
-    println("Ваш тип карты: $typeOfCard")
-    println("Сумма перевода с комиссией: $result")
 }
