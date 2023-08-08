5065f625168990ee51decd344617d9327e37fce3
Yuriy Chernov <Chernov.y.12@gmail.com>
2023-08-08 14:54:04 +0300

    Fourth Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,29 @@
 const val TYPE_MASTERCARD = "MasterCard"
 const val TYPE_MAESTRO = "Maestro"
 const val TYPE_VISA = "Visa"
-const val TYPE_MIR = "Mir"
+const val TYPE_MIR = "Мир"
 const val TYPE_VK_PAY = "VK Pay"
 const val ERROR_WRONG_TYPE = -1.0
 const val ERROR_LIMIT = -2.0
 
 
 fun main() {
-    val typeCard = "Visa"
-    val money = 10_000
-    val previous = 117_500
 
-    println(commission(typeCard, money, previous))
+    println(commission(typeCard = TYPE_MASTERCARD, money = 100_000, previous = 300_000))
 }
 
-fun commission(typeCard: String = "тип", money: Int = 0, previous: Int = 0): Any {
+fun commission(typeCard: String = "VK Pay", money: Int, previous: Int): Any {
     return when (typeCard) {
-        TYPE_MASTERCARD, TYPE_MAESTRO -> if (money > 150_000 || money + previous > 600_000) {
+        TYPE_MASTERCARD, TYPE_MAESTRO -> if (money + previous > 150_000 || money + previous > 600_000) {
             ERROR_LIMIT
-        } else if (money in 300..75_000) 0.00 + previous else ((money * 0.006) + 20) + previous
+        } else if ((money + previous) in 300..75_000) 0.00 else if (previous > 75_000) (money * 0.006) + 20
+        else ((previous + money - 75_000) * 0.006 + 20)
 
-        TYPE_VISA, TYPE_MIR -> if (money > 150_000 || money + previous > 600_000) {
+        TYPE_VISA, TYPE_MIR -> if (money > 150_000 || money > 600_000) {
             ERROR_LIMIT
-        } else if ((money * 0.075) < 35) (money + 35) + previous else (money + (money * 0.075)) + previous
+        } else if ((money * 0.075) < 35) (money + 35) else (money + (money * 0.075))
 
-        TYPE_VK_PAY -> if (money > 15_000 || money + previous > 40_000) {
+        TYPE_VK_PAY -> if (money > 15_000 || money > 40_000) {
             ERROR_LIMIT
         } else money
 
